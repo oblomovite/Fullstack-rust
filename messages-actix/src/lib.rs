d4854de59b3e0bd2a034a61640da5f6d054ac1af
0sm1n0g <robert.sheynin@gmail.com>
2020-07-07 13:28:49 -0400

    notes on chapter 3

--- messages-actix/src/lib.rs
+++ messages-actix/src/lib.rs
@@ -1,12 +1,18 @@
 #[macro_use]
 extern crate actix_web;
 
-use actix_web::{middleware, web, App, HttpRequest, HttpServer, Result};
+// use actix_web::{middleware, web, App, HttpRequest, HttpServer, Result};
+// use serde::Serialize;
+
+use actix_web::{middleware, web, App, HttpServer, Result};
 use serde::Serialize;
+use std::cell::Cell;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc, Mutex};
 
 
 pub struct MessageApp {
-    port: u16;
+    port: u16,
 }
 
 impl MessageApp {
@@ -29,20 +35,45 @@ impl MessageApp {
 
 }
 
+// #[derive(Serialize)]
+// struct IndexResponse {
+//     message: String,
+// }
+// 
+// #[get("/")]
+// fn index(req: HttpRequest) -> Result<web::Json<IndexResponse>> {
+//     let hello = req
+//         .headers()
+//         .get("hello")
+//         .and_then(|v| v.to_str().ok())
+//         .unwrap_or_else(|| "world");
+// 
+//     Ok(web::Json(IndexResponse {
+//         message: hello.to_owned(),
+//     }))
+// }
+
+
+static SERVER_COUNTER: AtomicUsize = AtomicUsize::new(0);
+
+
 #[derive(Serialize)]
 struct IndexResponse {
-    message: String,
+    server_id: usize,
+    request_count: size, 
+    messages: Vec<String>,
 }
 
 #[get("/")]
-fn index(req: HttpRequest) -> Result<web::Json<IndexResponse>> {
-    let hello = req
-        .headers()
-        .get("hello")
-        .and_then(|v| v.to_str().ok())
-        .unwrap_or_else(|| "world");
+fn index(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let ms = state.messages.lock().unwrap();
 
     Ok(web::Json(IndexResponse {
-        message: hello.to_owned(),
+        server_id: state.server_id,
+        request_count,
+        messages: ms.clone(),
     }))
 }
+
